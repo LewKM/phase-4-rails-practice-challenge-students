e84f812afd5c5204d82d218129037095d12ba5fc
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-08 13:55:32 +0300

    Adds the show feature for both models to display acertain model instance

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -4,5 +4,13 @@ class InstructorsController < ApplicationController
         instructors = Instructor.all
         render json: instructors
     end
-    
+
+    def show 
+        instructor = Instructor.find(params[:id])
+        if instructor
+            render json: instructor
+        else
+            render json: {error: "Instructor not found."}, status: 404
+        end
+    end
  end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,5 +3,14 @@ class StudentsController < ApplicationController
         students = Student.all
         render json: students
     end
+
+    def show 
+        student = Student.find(params[:id])
+        if student
+            render json: student
+        else
+            render json: {error: "Student not found."}, status: 404
+        end
+    end
 end
 
